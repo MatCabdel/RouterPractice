b26c335396b6410825c05b9717af18199cbd748b
Mathieu Chauveau <chauveau.math@gmail.com>
2024-04-15 17:03:20 +0200

    atelier ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DirectivesComponent } from './components/directives/directives.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "directives", component: DirectivesComponent},
+  {path: "aboutus", component: AboutusComponent},
+  {path: "**", redirectTo: '/page-not-found'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DirectivesComponent } from './components/directives/directives.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DirectivesComponent,
+    AboutusComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
